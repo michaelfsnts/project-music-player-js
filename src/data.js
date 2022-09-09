c401fb240d2757a37f32812df885f43edc33833a
Michael Ferreira dos Santos <64515823+michaelfsnts@users.noreply.github.com>
2022-09-09 13:49:52 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default[
+    {
+      title: "Go Down Swinging (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Go Down Swinging (Instrumental) - NEFFEX.mp3"
+    },
+    {
+      title: "Hustlin' (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Hustlin' (Instrumental) - NEFFEX.mp3"
+    },
+    {
+      title: "Just Breathing (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Just Breathing (Instrumental) - NEFFEX.mp3"
+    },
+    {
+        title: "That's What It Takes (Instrumental)",
+        artist: "NEFFEX",
+        cover: "imagemPadrao.png",
+        file: "That's What It Takes (Instrumental) - NEFFEX.mp3"
+      }
+    
+  ];
+  
